02cd20006776ade50ef167b8b534d7d4a1650315
sarswatji95 <mohitsar9@gmail.com>
2025-12-26 10:34:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram.ext import (
     filters
 )
 
-BOT_TOKEN = os.getenv("7862802172:AAFJ4tJ4aIBEDZ-jrW1yptvhEC-uyPhyx5U")
+BOT_TOKEN = os.getenv("8499256039:AAF39IcPuK47IaDk50nUKul4TcHnNvfoORY")
 
 def is_paid(user_id):
     if not os.path.exists("users.json"):
