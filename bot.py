33f4a1dfa063ffac65881cbea8420f350e832019
sarswatji95 <mohitsar9@gmail.com>
2025-12-26 08:58:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Filters
 import os
 import json
 from datetime import datetime
